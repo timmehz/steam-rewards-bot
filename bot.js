129e26efe78207cc99f58eb2a4810f5863675765
BrandonRoss <crazyserver00124@gmail.com>
2019-02-21 11:40:30 -0600

    test 2

--- bot.js
+++ bot.js
@@ -1,29 +1,15 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
-
- 
 
 client.on('message', message => {
-
     if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+    	message.reply('pong');
+  	}
 });
 
- 
-
 // THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.NTE1MzE3NjEwNzE2NDYzMTA0.D0pRqg.iOGsOTYK5D5nO6R-Mr7s3Asb6Ng);//where BOT_TOKEN is the token of our bot
+client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
